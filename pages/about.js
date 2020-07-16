d4ac238ac725c04cc23897fceb917e7982ca639b
Adam Kaewell <akaewell@xometry.com>
2020-07-16 11:34:25 -0400

    fetching data with getInitialProps

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,24 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
+import fetch from "isomorphic-unfetch";
+import { Component } from "react";
 
-export default () => (
-  <Layout title="About">
-    <h1>About</h1>
-    <Link href="/">
-      <a>Go to Home</a>
-    </Link>
-    <p>A JavaScript Programmer</p>
-    <img src="mm-purple.png" alt="JavaScript" height="200px" />
-  </Layout>
-);
+export default class About extends Component {
+  static async getInitialProps() {
+    const res = await fetch("https://api.github.com/users/a2k11");
+    const data = await res.json();
+
+    return { user: data };
+  }
+
+  render() {
+    const { user } = this.props;
+
+    return (
+      <Layout title="About">
+        <p>{user.name}</p>
+        <img src={user.avatar_url} alt="Amm" height="200px" />
+      </Layout>
+    );
+  }
+}
